e38604ada00b3b8c29a7b8eebb5d8930cf3ef67a
KenjiSpecial <k.saito.1985@gmail.com>
2012-11-18 22:15:37 +0100

    my work @home Nov 18

---
+++ example/6_collision/5_collision_circle_two_dimension/script.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: saitoukenji
+ * Date: 11/18/12
+ * Time: 5:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ example/6_collision/6_collision/script.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: saitoukenji
+ * Date: 11/18/12
+ * Time: 9:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
